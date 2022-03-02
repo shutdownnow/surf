b5637823b6c4965a7e88bcb25681951be9e680f2
Nobume <923551363@qq.com>
2022-03-02 21:48:00 +0800

    homepage

--- config.h
+++ config.h
@@ -242,4 +242,4 @@ static Button buttons[] = {
 	{ OnMedia,      MODKEY,         1,      clickexternplayer, { 0 },       1 },
 };
 
-#define HOMEPAGE "https://cn.bing.com"
+#define HOMEPAGE "/www/index.html"
